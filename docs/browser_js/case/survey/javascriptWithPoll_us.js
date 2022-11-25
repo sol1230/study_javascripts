c5c48bcf2bb925cd85a63cff44b6229eb339331d
sol1230 <solllll1230@gmail.com>
2022-11-25 09:02:16 +0900

    add

--- docs/browser_js/case/survey/javascriptWithPoll_us.js
+++ docs/browser_js/case/survey/javascriptWithPoll_us.js
@@ -2,31 +2,31 @@
 let questions = [
   {
     question_uid: "Q1",
-    question: "1. 해당 매장 방문 시 매장은 청결하였습니까?",
+    question: "해당 매장 방문 시 매장은 청결하였습니까?",
     order: "1",
     answer_u: "E1 E2",
   },
   {
     question_uid: "Q2",
-    question: "2. 직원이 제조한 음료는 맛이 좋았습니까?",
+    question: "직원이 제조한 음료는 맛이 좋았습니까?",
     order: "2",
     answer_u: "E1 E2 E3 E4",
   },
   {
     question_uid: "Q3",
-    question: "3. 주문시 직원은 고객님께 친절하였습니까?",
+    question: "주문시 직원은 고객님께 친절하였습니까?",
     order: "3",
     answer_u: "E1 E2",
   },
   {
     question_uid: "Q4",
-    question: "4. 해당 매장을 다음에도 재방문 하실 의향이 있으십니까?",
+    question: "해당 매장을 다음에도 재방문 하실 의향이 있으십니까?",
     order: "4",
     answer_u: "E1 E2 E3 E4 E5",
   },
   {
     question_uid: "Q5",
-    question: "5. 주문하신 음료가 나오기까지 시간이 적당하였습니까?",
+    question: "주문하신 음료가 나오기까지 시간이 적당하였습니까?",
     order: "5",
     answer_u: " E1 E2 E3",
   },
@@ -70,24 +70,76 @@ let queAnsCnn = [
 // ]
 // 1차 : [Q1, Q2, Q3, Q4, Q5]
 // 2차 : Array in Array [[Q1, E1, E2], ...]
+// 3차 : Object in Array [{question_uid: Q1, answer_uid: [E1, E2]}, ...]
+
 let polls = []; // 전체 묶음
 let question_compare;
-let question_a = []; // 내부 묶음
+let question_a = {}; // 내부 묶음
+let answer_a = []; // 내부 설문 답변 묶음
 for (let idx = 0; idx < queAnsCnn.length; idx++) {
   if (question_compare != queAnsCnn[idx]["question_uid"]) {
-    if (question_a.length > 0) {
+    if (Object.keys(question_a).length > 0) {
+      question_a["answer_uid"] = answer_a;
       polls.push(question_a);
-      question_a = [];
+      question_a = {};
+      answer_a = [];
     }
     // console.log(`!== : ${queAnsCnn[idx]["question_uid"]}`);
     // console.log(`!== : ${queAnsCnn[idx]["answer_uid"]}`);
-    question_a.push(queAnsCnn[idx]["question_uid"]);
-    question_a.push(queAnsCnn[idx]["answer_uid"]);
+    // question_a.push(queAnsCnn[idx]["question_uid"]);
+    question_a["question_uid"] = queAnsCnn[idx]["question_uid"];
+    answer_a.push(queAnsCnn[idx]["answer_uid"]);
   } else {
     // console.log(`== : ${queAnsCnn[idx]["answer_uid"]}`);
-    question_a.push(queAnsCnn[idx]["answer_uid"]);
+    answer_a.push(queAnsCnn[idx]["answer_uid"]);
+    if (idx + 1 >= queAnsCnn.length) {
+      question_a["answer_uid"] = answer_a;
+      polls.push(question_a);
+    }
   }
   question_compare = queAnsCnn[idx]["question_uid"]; // 이전 uid 입력
 }
-polls.push(question_a); // Q5
-console.log(`${polls}`);
+// polls.push(question_a); // Q5
+// console.log(`${polls}`);
+
+// 출력
+// [{question_uid: Q1, answer_uid: [E1, E2], ...}]
+// polls[0]["question_uid"];
+// polls[0]["answer_uid"][0];
+// polls[0]["answer_uid"][1];
+
+// polls[1]["question_uid"];
+// polls[1]["answer_uid"][0];
+// polls[1]["answer_uid"][1];
+// polls[1]["answer_uid"][2];
+
+// 설문 문항 가져오는 함수
+function getQuestionsByUid(question_uid) {
+  let question_desc;
+  for (ques of questions) {
+    if (ques["question_uid"] == question_uid) {
+      question_desc = ques["question"];
+    }
+  }
+  return question_desc;
+}
+
+function getAnswersByUid(answer_uid) {
+  let answer_desc;
+  for (ans of answers) {
+    if (ans["answer_uid"] == answer_uid) {
+      answer_desc = ans["answer"];
+    }
+  }
+  return answer_desc;
+}
+
+for (poll of polls) {
+  console.log(
+    `${poll["question_uid"]}. ${getQuestionsByUid(poll["question_uid"])}`
+  ); // == polls[idx]
+  let answer_uids = poll["answer_uid"];
+  answer_uids.forEach((answer_uid, index) => {
+    console.log(`${index + 1}.${getAnswersByUid(answer_uid)}`);
+  });
+}
