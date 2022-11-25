7d3e1a64e0337eda8cc62913bcb6f1506236218a
sol1230 <solllll1230@gmail.com>
2022-11-25 12:33:10 +0900

    add radio

--- docs/browser_js/case/survey/javascriptWithPoll_us.js
+++ docs/browser_js/case/survey/javascriptWithPoll_us.js
@@ -34,11 +34,11 @@ let questions = [
 
 // 답항
 let answers = [
-  { answer_uid: "E1", answer: "(1) 전혀 아니다", order: "1" },
-  { answer_uid: "E2", answer: "(2) 아니다", order: "2" },
-  { answer_uid: "E3", answer: "(3) 보통이다", order: "3" },
-  { answer_uid: "E4", answer: "(4) 그렇다", order: "4" },
-  { answer_uid: "E5", answer: "(5) 매우 그렇다", order: "5" },
+  { answer_uid: "E1", answer: "전혀 아니다", order: "1" },
+  { answer_uid: "E2", answer: "아니다", order: "2" },
+  { answer_uid: "E3", answer: "보통이다", order: "3" },
+  { answer_uid: "E4", answer: "그렇다", order: "4" },
+  { answer_uid: "E5", answer: "매우 그렇다", order: "5" },
 ];
 
 // 문항 - 답항 연결
@@ -117,29 +117,94 @@ for (let idx = 0; idx < queAnsCnn.length; idx++) {
 function getQuestionsByUid(question_uid) {
   let question_desc;
   for (ques of questions) {
-    if (ques["question_uid"] == question_uid) {
+    if (ques["question_uid"] === question_uid) {
+      // === : 값, 데이터 타입까지 비교
       question_desc = ques["question"];
+      break;
     }
   }
   return question_desc;
 }
 
+// 설문 답 문항 가져오는 함수
 function getAnswersByUid(answer_uid) {
   let answer_desc;
   for (ans of answers) {
-    if (ans["answer_uid"] == answer_uid) {
+    if (ans["answer_uid"] === answer_uid) {
       answer_desc = ans["answer"];
+      break;
     }
   }
   return answer_desc;
 }
 
 for (poll of polls) {
-  console.log(
-    `${poll["question_uid"]}. ${getQuestionsByUid(poll["question_uid"])}`
-  ); // == polls[idx]
+  // console.log(
+  //   `${poll["question_uid"]}. ${getQuestionsByUid(poll["question_uid"])}`
+  // ); // == polls[idx]
   let answer_uids = poll["answer_uid"];
   answer_uids.forEach((answer_uid, index) => {
-    console.log(`${index + 1}.${getAnswersByUid(answer_uid)}`);
+    // console.log(`${index + 1}.${getAnswersByUid(answer_uid)}`);
   });
+}
+
+// Event handles next 누르면 순서대로 출력
+// 대상 변수는 polls
+
+let queryNext = document.querySelector("#next");
+queryNext.addEventListener("click", setPollContents);
+// if (index == 4) {
+//   console.log("마지막 페이지 입니다.");
+// }
+
+let index = 0;
+
+function setPollContents() {
+  if (index == polls.length) {
+    alert("마지막 페이지 입니다.");
+    return;
+  } else {
+    let queryContents = document.querySelector("#poll-contents");
+    // polls[0]["question_uid"]   // 설문 문항
+    // polls[0]["answer_uid"]   // 설문 답 문항
+    // console.log(getQuestionsByUid(polls[index]["question_uid"]));
+    let desc = `<div>${index + 1}. ${getQuestionsByUid(
+      polls[index]["question_uid"]
+    )}</div>`;
+    polls[index]["answer_uid"].forEach((answer_a, index) => {
+      // console.log(`${index + 1}. ${getAnswersByUid(answer_a)}`);
+      desc =
+        desc +
+        `<div><input type = "radio" name = "answer" "id = "${index}"><label for = "${index}">(${
+          index + 1
+        }). ${getAnswersByUid(answer_a)}</label></div>`;
+    });
+
+    queryContents.innerHTML = desc;
+    index++;
+  }
+}
+
+let queryPrev = document.querySelector("#prev");
+queryPrev.addEventListener("click", setPollPrev);
+
+function setPollPrev() {
+  if (index == 1) {
+    alert("이전 페이지가 없습니다.");
+    return;
+  } else {
+    let queryContentsMin = document.querySelector("#poll-contents");
+    let prev = `<div>${index - 1}. ${getQuestionsByUid(
+      polls[index - 2]["question_uid"]
+    )}</div>`;
+    polls[index - 2]["answer_uid"].forEach((answer_a, index) => {
+      prev =
+        prev +
+        `<div><input type = "radio" id = "${index}" name = "answer"><label for = "${index}">(${
+          index + 1
+        }). ${getAnswersByUid(answer_a)}</label></div>`;
+    });
+    queryContentsMin.innerHTML = prev;
+    index--;
+  }
 }
